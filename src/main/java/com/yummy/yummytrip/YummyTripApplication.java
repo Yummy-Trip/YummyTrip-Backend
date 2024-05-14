a924c490835db54bd35ab242406370297dcaccd4
이용재 <dydwo0740@naver.com>
2024-05-14 14:00:24 +0900

    first commit

---
+++ src/main/java/com/yummy/yummytrip/YummyTripApplication.java
@@ -0,0 +1,13 @@
+package com.yummy.yummytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yummy/yummytrip/YummyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yummy.yummytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
