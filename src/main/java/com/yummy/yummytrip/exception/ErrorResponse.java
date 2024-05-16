7c5be43beeb3324bb7215936e9d1d026c09c48f1
이용재 <dydwo0740@naver.com>
2024-05-16 16:05:21 +0900

    :sparkles: 이미 존재하는 Email이 있을시에 중복검사를 위한 예외처리 클래스를 구현합니다. #1

---
+++ src/main/java/com/yummy/yummytrip/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.yummy.yummytrip.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ErrorResponse {
+    private final String code;
+    private final String message;
+}
