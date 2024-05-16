9a05805adc134874387e4e649118a801cb810ddc
이용재 <dydwo0740@naver.com>
2024-05-16 16:04:48 +0900

    :sparkles: UserService를 구현합니다. #1

---
+++ src/main/java/com/yummy/yummytrip/user/service/UserService.java
@@ -0,0 +1,36 @@
+package com.yummy.yummytrip.user.service;
+
+import com.yummy.yummytrip.exception.EmptyDataException;
+import com.yummy.yummytrip.exception.ErrorCode;
+import com.yummy.yummytrip.user.mapper.UserMapper;
+import com.yummy.yummytrip.user.model.JoinDto;
+import com.yummy.yummytrip.user.model.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserMapper mapper;
+    private final BCryptPasswordEncoder encoder;
+
+    public void signUp(JoinDto joinUser) {
+
+        UserDto findUser = mapper.findByEmail(joinUser.getEmail());
+
+        if (findUser != null) {
+            throw new EmptyDataException(ErrorCode.DATA_NOT_EXISTS);
+        }
+
+        UserDto user = UserDto.builder()
+                .username(joinUser.getUsername())
+                .email(joinUser.getEmail())
+                .password(encoder.encode(joinUser.getPassword()))
+                .birth(joinUser.getBirth())
+                .usertype(joinUser.getUsertype())
+                .build();
+
+        mapper.signUp(user);
+    }
+}
