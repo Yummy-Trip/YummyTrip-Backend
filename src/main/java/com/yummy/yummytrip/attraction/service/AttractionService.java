aa3ad2896af9a59beb16872ce77a35b7d6fb2d03
fsm12 <fsm1204@naver.com>
2024-05-17 18:39:38 +0900

    :sparkles: 관광지 단일 및 전체 조회에 대해 mapper에서 조회해온 값을 넘겨주는 로직 작성 (#3)

--- src/main/java/com/yummy/yummytrip/attraction/service/AttractionService.java
+++ src/main/java/com/yummy/yummytrip/attraction/service/AttractionService.java
@@ -1,17 +1,28 @@
 package com.yummy.yummytrip.attraction.service;
 
 import com.yummy.yummytrip.attraction.mapper.AttractionMapper;
+import com.yummy.yummytrip.attraction.model.AttractionSearchRequestDto;
 import com.yummy.yummytrip.attraction.model.GetAttractionResponseDto;
 import com.yummy.yummytrip.exception.CustomException;
 import com.yummy.yummytrip.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
 public class AttractionService {
     private final AttractionMapper mapper;
+
+    public GetAttractionResponseDto getAttractionById(Long attractionId) {
+        return findAttraction(attractionId);
+    }
+
+    public List<GetAttractionResponseDto> getAttractionByFiltering(AttractionSearchRequestDto searchDto) {
+        return mapper.getAttractionByFiltering(searchDto);
+    }
 
     private GetAttractionResponseDto findAttraction(Long attractionId) {
         return mapper.getAttractionById(attractionId)
