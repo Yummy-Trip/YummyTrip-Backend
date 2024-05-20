0d783bc837f7ee21bbd0268a554333d2194a9d4f
박소미 <fsm1204@naver.com>
2024-05-20 12:32:05 +0900

    ✨ 각 api에 대해 swagger에서 보여줄 태그와 설명 추가 (#12)

--- src/main/java/com/yummy/yummytrip/notice/controller/NoticeController.java
+++ src/main/java/com/yummy/yummytrip/notice/controller/NoticeController.java
@@ -4,12 +4,15 @@ import com.yummy.yummytrip.notice.model.GetNoticeResponseDto;
 import com.yummy.yummytrip.notice.model.NoticeCreateRequestDto;
 import com.yummy.yummytrip.notice.model.NoticeUpdateRequestDto;
 import com.yummy.yummytrip.notice.service.NoticeService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 public class NoticeController {
     private final NoticeService noticeService;
 
+    @Operation(summary = "Add notice.", description = "공지사항 작성")
     @PostMapping("/add")
     public ResponseEntity<Boolean> createNotice(
             @RequestBody NoticeCreateRequestDto requestDto
@@ -26,6 +30,7 @@ public class NoticeController {
         return ResponseEntity.ok(noticeService.createNotice(requestDto));
     }
 
+    @Operation(summary = "Get information on just one notice.", description = "공지사항 정보 조회 (단일 아이디)")
     @GetMapping("/{noticeId}")
     public ResponseEntity<GetNoticeResponseDto> getNoticeById(
             @PathVariable("noticeId") Long noticeId
@@ -33,11 +38,13 @@ public class NoticeController {
         return ResponseEntity.ok(noticeService.getNoticeById(noticeId));
     }
 
+    @Operation(summary = "Get all notices.", description = "공지사항 정보 전체 조회")
     @GetMapping("/get")
     public ResponseEntity<List<GetNoticeResponseDto>> getNotices(){
         return ResponseEntity.ok(noticeService.getNotices());
     }
 
+    @Operation(summary = "Update notice By noticeId", description = "공지사항 정보 전체 조회")
     @PostMapping("/upadate")
     public ResponseEntity<Long> updateNotice(
             @RequestBody NoticeUpdateRequestDto requestDto
@@ -45,6 +52,7 @@ public class NoticeController {
         return ResponseEntity.ok(noticeService.updateNotice(requestDto));
     }
 
+    @Operation(summary = "Delete notice By noticeId", description = "공지사항 삭제")
     @DeleteMapping("/delete/{noticeId}")
     public ResponseEntity<Void> deleteNotice(
             @PathVariable("noticeId") Long noticeId
