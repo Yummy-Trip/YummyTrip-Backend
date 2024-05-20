a22ad44ee2b02a3b53a672625203f1d2a16d7021
박소미 <fsm1204@naver.com>
2024-05-20 12:34:41 +0900

    ✨ 공지사항 업데이트 요청 Dto 생성 (#12)

--- src/main/java/com/yummy/yummytrip/notice/model/NoticeUpdateRequestDto.java
+++ src/main/java/com/yummy/yummytrip/notice/model/NoticeUpdateRequestDto.java
@@ -1,4 +1,14 @@
 package com.yummy.yummytrip.notice.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class NoticeUpdateRequestDto {
+    private Long noticeId;
+    private String title;
+    private String content;
 }
