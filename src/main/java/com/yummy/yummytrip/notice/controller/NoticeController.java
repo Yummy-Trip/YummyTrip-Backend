888528f69e1d9db98e136d9e44e512f31be727ca
박소미 <fsm1204@naver.com>
2024-05-20 12:27:58 +0900

    ✨ 컨트롤러에서 사용자 아이디와 제목, 내용으로 공지사항 생성 메소드 추가 (#12)

--- src/main/java/com/yummy/yummytrip/notice/controller/NoticeController.java
+++ src/main/java/com/yummy/yummytrip/notice/controller/NoticeController.java
@@ -1,9 +1,12 @@
 package com.yummy.yummytrip.notice.controller;
 
+import com.yummy.yummytrip.notice.model.NoticeCreateRequestDto;
+import com.yummy.yummytrip.notice.service.NoticeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,5 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/notice")
 @Tag(name = "2. Notice", description = "공지사항 정보 CRUD")
 public class NoticeController {
+    private final NoticeService noticeService;
 
+    @PostMapping("/add")
+    public ResponseEntity<Boolean> createNotice(
+            @RequestBody NoticeCreateRequestDto requestDto
+    ){
+        return ResponseEntity.ok(noticeService.createNotice(requestDto));
+    }
 }
