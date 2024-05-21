7d2bcc6fed03eb5fa47a10bf7e83e9e63c93e56f
박소미 <fsm1204@naver.com>
2024-05-21 10:08:26 +0900

    :sparkles: service에서 사용자 아이디를 받아 새로운 Dto를 만들어 mapper로 넘겨주는 비즈니스 로직 작성 (#13)

--- src/main/java/com/yummy/yummytrip/notice/service/NoticeService.java
+++ src/main/java/com/yummy/yummytrip/notice/service/NoticeService.java
@@ -6,6 +6,8 @@ import com.yummy.yummytrip.notice.mapper.NoticeMapper;
 import com.yummy.yummytrip.notice.model.GetNoticeResponseDto;
 import com.yummy.yummytrip.notice.model.NoticeCreateRequestDto;
 import com.yummy.yummytrip.notice.model.NoticeUpdateRequestDto;
+import com.yummy.yummytrip.user.mapper.UserMapper;
+import com.yummy.yummytrip.user.model.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +17,18 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
-    private final NoticeMapper mapper;
+    private final NoticeMapper noticeMapper;
+    private final UserMapper userMapper;
 
-    public Boolean createNotice(NoticeCreateRequestDto requestDto) {
-        return mapper.insertNotice(requestDto) > 0;
+    public Boolean createNotice(NoticeCreateRequestDto requestDto, String email) {
+        UserDto userDto = userMapper.findByEmail(email);
+        GetNoticeResponseDto mapperDto = GetNoticeResponseDto
+                .builder()
+                .title(requestDto.getTitle())
+                .content(requestDto.getContent())
+                .userId(userDto.getUserId())
+                .build();
+        return noticeMapper.insertNotice(mapperDto) > 0;
     }
 
     public GetNoticeResponseDto getNoticeById(Long noticeId) {
@@ -26,19 +36,19 @@ public class NoticeService {
     }
 
     public List<GetNoticeResponseDto> getNotices() {
-        return mapper.selectAllNotices();
+        return noticeMapper.selectAllNotices();
     }
 
     public void updateNotice(NoticeUpdateRequestDto requestDto) {
-        mapper.updateNotice(requestDto);
+        noticeMapper.updateNotice(requestDto);
     }
 
     public void deleteNotice(Long noticeId) {
-        mapper.deleteNotice(noticeId);
+        noticeMapper.deleteNotice(noticeId);
     }
 
     private GetNoticeResponseDto findNotice(Long noticeId) {
-        return mapper.getNoticeById(noticeId)
+        return noticeMapper.getNoticeById(noticeId)
                 .orElseThrow(() -> new CustomException(ErrorCode.DATA_NOT_EXISTS, "공지사항이 존재하지 않습니다. (NoticeService)"));
     }
 }
